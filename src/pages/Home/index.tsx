d7a1b0090c4c33c23cb13809b71bb2ea9d9be576
AndersonPGS <andersonpgs2001@hotmail.com>
2022-04-09 20:44:02 -0300

    home page base created

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./styles";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Container></Container>
+    </div>
+  );
+};
+
+export default Home;
