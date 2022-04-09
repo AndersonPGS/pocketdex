11c8f14f735460f3326dc801ffdd7451e1ebd265
AndersonPGS <andersonpgs2001@hotmail.com>
2022-04-09 20:44:32 -0300

    GlobalStyles Created

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html {
+    min-height: 100%;
+    background-color: var(--primary);
+  }
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: "Inter",-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+    color: var(--text-primary)
+  }
+  ul {
+    list-style: none;
+  }
+
+  :root {
+    --primary: #1F1D2B;
+    --text-primary: #C5C1D2;
+    --text-secondary: #969599;
+    --bar-background: #C4C4C4;
+    --purple: #9750E8;
+    --red: #E25E5E;
+    --yellow: #D8E25E;
+    --orange: #E2B55E;
+    --green: #A0E25E;
+  }
+`;
