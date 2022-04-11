b6f13cf189835c08080ac2656ebf9dc22e98e709
AndersonPGS <andersonpgs2001@hotmail.com>
2022-04-11 12:10:04 -0300

    Href edited

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,12 +5,12 @@ const Footer: React.FC = () => {
   return (
     <Container>
       <div className="left">
-        <a href="/">Created by AndersonPGS</a>
+        <a href="https://github.com/andersonpgs">Created by AndersonPGS</a>
       </div>
       <div className="right">
-        <a href="/">API</a>
-        <a href="/">Contact</a>
-        <a href="/">Github</a>
+        <a href="https://pokeapi.co/docs/">API</a>
+        <a href="mailto:andersonpgs.contato@hotmail.com">Contact</a>
+        <a href="https://github.com/andersonpgs/pocketdex">Github</a>
       </div>
     </Container>
   );
